d0d149deda70056c5cfafa7ea8206bac0afced3d
Spec-Chum <specchumster@gmail.com>
2018-05-18 15:22:28 +0100

    32-bit SFML audio dll appears to be broken so unticked prefer 32-bit on Build configuration.  Also changed order of SoundBuffer constrcutor params to now be correct as had to use work around before for broken dll

--- Chip8/Audio.cs
+++ Chip8/Audio.cs
@@ -19,7 +19,7 @@ namespace Chip8
                 raw[i] = (short)(((i / (44100 / 256) / 2) % 2) == 1 ? 10000 : -10000);
             }
 
-            buffer = new SoundBuffer(raw, 44100, 1);
+            buffer = new SoundBuffer(raw, 1, 44100);
             sound = new Sound(buffer);
         }
 
